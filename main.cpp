c3d0ae7d507fecfc75330914acab77c5629c69b4
Evan Overman <evanrileyoverman@gmail.com>
2020-12-14 14:45:13 -0800

    added openssl source

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <openssl/rsa.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
 
-#include "openssl_rsa.h"
+#include "openssl/rsa.h" // <openssl.rsa.h>
+#include "openssl/pem.h" // <openssl/pem.h>
+#include "openssl/err.h" // <openssl/err.h>
+#include "rsa.hpp"
 
 int main (int argc, const char *argv[]) 
 {
-    LOG("OpenSSL_RSA has been started.");
+    std::clog << "OpenSSL_RSA has been started.\n";
     
     RSA *private_key;
     RSA *public_key;
@@ -19,10 +20,10 @@ int main (int argc, const char *argv[])
     bool do_decrypt = false;
     bool do_encrypt = false;
 
-    std::string encrypted_file = "encrypted_file.bin";
-    std::string outfile = "decrypted_file.txt"
+    std::string encrypted_file = "encrypted_file.txt";
+    std::string outfile = "decrypted_file.txt";
 
-    for (count = 0; count < argc; count++)
+    for (int count = 0; count < argc; count++)
     {
         if (std::string(argv[count]) == "/r") // Private Key
         {
@@ -54,58 +55,70 @@ int main (int argc, const char *argv[])
         
     }
 
-    char message[KEY_LENGTH / 8] = "Batuhan AVLAYAN - OpenSSL_RSA demo";
+    std::string file = std::string(argv[argc - 1]);
+    std::string line;
+    std::string message;
+
+    std::fstream infile(file, std::fstream::in);
+
+    while (getline(infile, line))
+    {
+        message += line + "\n";
+    }
+
+    // char message[KEY_LENGTH / 8] = "Batuhan AVLAYAN - OpenSSL_RSA demo";
     char *encrypt = NULL;
-    char *decrypt = NULL;
+    char *decrypt = NULL;    
 
-    
-
-    LOG(KEY_LENGTH);
-    LOG(PUBLIC_EXPONENT);
+    std::clog << KEY_LENGTH << "\n";
+    std::clog << PUBLIC_EXPONENT << "\n";
     
     RSA *keypair = RSA_generate_key(KEY_LENGTH, PUBLIC_EXPONENT, NULL, NULL);
-    LOG("Generate key has been created.");
+    std::clog << "Generate key has been created.\n";
 
-    private_key = create_RSA(keypair, PRIVATE_KEY_PEM, private_key_pem);
-    LOG("Private key pem file has been created.");
+    private_key = rsa::create_RSA(keypair, PRIVATE_KEY_PEM, private_key_pem);
+    std::clog << "Private key pem file has been created.\n";
 
-    public_key  = create_RSA(keypair, PUBLIC_KEY_PEM, public_key_pem);
-    LOG("Public key pem file has been created.");;
+    public_key  = rsa::create_RSA(keypair, PUBLIC_KEY_PEM, public_key_pem);      
+    std::clog << "Public key pem file has been created.\n";
 
     encrypt = (char*)malloc(RSA_size(public_key));
-    int encrypt_length = public_encrypt(strlen(message) + 1, (unsigned char*)message, (unsigned char*)encrypt, public_key, RSA_PKCS1_OAEP_PADDING);
+    int encrypt_length = rsa::public_encrypt(strlen(message.c_str()) + 1, (unsigned char*)message.c_str(), (unsigned char*)encrypt, public_key, RSA_PKCS1_OAEP_PADDING);
 
     if (encrypt_length == -1) 
     {
-        LOG("An error occurred in public_encrypt() method");
+        std::cerr << "An error occurred in public_encrypt() method\n";
+        return 1;
     }
 
-    LOG("Data has been encrypted.");
+    std::clog << "Data has been encrypted.\n";
 
-    create_encrypted_file(encrypt, public_key);
-    LOG("Encrypted file has been created.");
+    rsa::create_encrypted_file(encrypt, public_key);
+    std::clog << "Encrypted file has been created.\n";
 
     decrypt = (char *)malloc(encrypt_length);
-    int decrypt_length = private_decrypt(encrypt_length, (unsigned char*)encrypt, (unsigned char*)decrypt, private_key, RSA_PKCS1_OAEP_PADDING);
+    int decrypt_length = rsa::private_decrypt(encrypt_length, (unsigned char*)encrypt, (unsigned char*)decrypt, private_key, RSA_PKCS1_OAEP_PADDING);
 
     if (decrypt_length == -1) 
     {
-        LOG("An error occurred in private_decrypt() method");
+        std::cerr << "An error occurred in private_decrypt() method\n";
+        return 1;
     }
 
-    LOG("Data has been decrypted.");
+    std::clog << "Data has been decrypted.\n";
 
-    FILE *decrypted_file = fopen("decrypted_file.txt", "w");
+    FILE *decrypted_file = fopen(outfile.c_str(), "w");
     fwrite(decrypt, sizeof(*decrypt), decrypt_length - 1, decrypted_file);
     fclose(decrypted_file);
-    LOG("Decrypted file has been created.");
+    std::clog << "Decrypted file has been created.\n";
     
     RSA_free(keypair);
     free(private_key);
     free(public_key);
     free(encrypt);
     free(decrypt);
-    LOG("OpenSSL_RSA has been finished.");
+    std::clog << "OpenSSL_RSA has been finished.\n";
 
     return 0;
+
 }
